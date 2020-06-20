035bb6174754f6223fedee70304549131124c0df
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-06-20 16:46:36 +0200

    update meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Gatsby e-commerce app.`,
+    author: `@wmadfaa`,
   },
   plugins: [
     {
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `flower-store`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
